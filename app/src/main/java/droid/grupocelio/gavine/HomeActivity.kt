c2c7a00ec21ce720fdbfd8b3b5bdc745f6b6d583
rubenglam <rubenbermudezsaez@gmail.com>
2019-02-27 23:19:05 +0100

    Update Views

---
+++ app/src/main/java/droid/grupocelio/gavine/HomeActivity.kt
@@ -0,0 +1,62 @@
+package droid.grupocelio.gavine
+
+import android.graphics.Typeface
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import droid.grupocelio.gavine.fragments.HomeFragment
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        navigation_view.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        navigation_view.selectedItemId = R.id.navigation_home
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.navigation_view)
+        var txtviewTitle : TextView = findViewById(R.id.txtviewTitle)
+        txtviewTitle.typeface = Typeface.createFromAsset(this.assets, "fonts/GoogleSans-Medium.ttf")
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        item ->
+        when (item.itemId) {
+            R.id.navigation_esports -> {
+                txtviewTitle.setText(R.string.title_esports)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_champion -> {
+                txtviewTitle.setText(R.string.title_champion)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_home -> {
+                txtviewTitle.setText(R.string.title_home)
+                createFragmentHome()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_ranking -> {
+                txtviewTitle.setText(R.string.title_ranking)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_news -> {
+                txtviewTitle.setText(R.string.title_news)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    fun createFragmentHome() {
+        val transaction = getSupportFragmentManager().beginTransaction()
+        var fragment : androidx.fragment.app.Fragment = HomeFragment.newInstance("a", "n") as androidx.fragment.app.Fragment
+        transaction.replace(R.id.container, fragment)
+        //transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+}
