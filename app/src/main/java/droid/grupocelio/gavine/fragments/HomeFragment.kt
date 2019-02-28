6587494dc31e6d4ed5fb62b02b55cf493d078340
rubenglam <rubenbermudezsaez@gmail.com>
2019-02-28 23:07:25 +0100

    Update Home Fragment

--- app/src/main/java/droid/grupocelio/gavine/fragments/HomeFragment.kt
+++ app/src/main/java/droid/grupocelio/gavine/fragments/HomeFragment.kt
@@ -11,38 +11,13 @@ import android.view.ViewGroup
 import droid.grupocelio.gavine.R
 import kotlinx.android.synthetic.main.activity_home.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [HomeFragmentX.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [HomeFragmentX.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class HomeFragment : androidx.fragment.app.Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var listener: OnFragmentInteractionListener? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         var containerView = inflater.inflate(R.layout.fragment_home_fragment, container, false)
-        containerView.txtviewTitle.text = param1
         return containerView
     }
 
@@ -82,22 +57,9 @@ class HomeFragment : androidx.fragment.app.Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragmentX.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-                HomeFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+                HomeFragment()
     }
 }
