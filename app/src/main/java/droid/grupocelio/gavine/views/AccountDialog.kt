a362a6a572424876e054da0b195eaf6cd1cd982a
rubenglam <rubenbermudezsaez@gmail.com>
2019-03-07 22:57:13 +0100

    Dialog bug fixed

--- app/src/main/java/droid/grupocelio/gavine/views/AccountDialog.kt
+++ app/src/main/java/droid/grupocelio/gavine/views/AccountDialog.kt
@@ -37,7 +37,7 @@ class AccountDialog : DialogFragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater!!.inflate(R.layout.view_action_bar, container, false)
+        val view = inflater!!.inflate(R.layout.fragment_account_dialog, container, false)
 
         val btnCancel = view.findViewById<View>(R.id.dialog1) as Button
 
